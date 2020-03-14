1004997c3d8e1558f4b1d15ea970d6405dea08e5
Kelps Sousa Alux <kelps@outlook.com>
2020-03-14 09:32:39 -0300

    repro of the build problem

---
+++ TestTypeScript/wwwroot/js/app.ts
@@ -0,0 +1,8 @@
+﻿module SampleModule {
+    class SampleClass {
+        fullname: string;
+        constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+            this.fullname = firstName + " " + middleInitial + " " + lastName;
+        }
+    }
+}
